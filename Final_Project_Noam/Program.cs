5ec391481b0621116a4ab988fda665a26be02afa
N0am <shelkovitznoam@gmail.com>
2025-04-29 16:20:34 +0300

    Added builders

--- Final_Project_Noam/Program.cs
+++ Final_Project_Noam/Program.cs
@@ -1,3 +1,4 @@
+using Final_Project_Noam;
 using Final_Project_Noam.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<DBService>(sp => new DBService("Server=localhost;Port=3306;Database=lawyers;User=root;Password=Root1234"));
+builder.Services.AddSingleton<PersonService>();
+
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
